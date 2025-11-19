68961acb6494aadab339ec70fdded6735d5d2c51
Lý Ngọc Long <148007027+lnlonggg@users.noreply.github.com>
2025-11-19 19:54:11 +0700

    feat: Contact and Test

--- Nhom5_EnglishCenter/Controllers/HomeController.cs
+++ Nhom5_EnglishCenter/Controllers/HomeController.cs
@@ -1,8 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Threading.Tasks;
 using TrungTamAnhNgu.Web.Models;
 using TrungTamAnhNgu.Web.Services;
-using System.Threading.Tasks;
+using TrungTamAnhNgu.Web.ViewModels;
 
 namespace Nhom5_EnglishCenter.Controllers
 {
@@ -40,6 +41,33 @@ namespace Nhom5_EnglishCenter.Controllers
         public IActionResult Error()
         {
             return View(new { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        // GET: /Home/Contact
+        public IActionResult Contact(string? subject)
+        {
+            var model = new ContactViewModel();
+            if (!string.IsNullOrEmpty(subject))
+            {
+                model.Subject = subject;
+            }
+            return View(model);
+        }
+
+        // POST: /Home/Contact
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Tạm thời chỉ hiện thông báo thành công
+
+                TempData["SuccessMessage"] = "Cảm ơn bạn đã liên hệ! Chúng tôi sẽ gọi lại cho bạn sớm nhất.";
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
         }
     }
 }
