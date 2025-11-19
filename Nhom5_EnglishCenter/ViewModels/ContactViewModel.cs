b8355c18fca6f4d8b53d146a7655ac8a11c8ad78
Lý Ngọc Long <148007027+lnlonggg@users.noreply.github.com>
2025-11-19 19:33:36 +0700

    feat: contact

---
+++ Nhom5_EnglishCenter/ViewModels/ContactViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TrungTamAnhNgu.Web.ViewModels
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage = "Vui lòng nhập Họ và tên")]
+        [Display(Name = "Họ và Tên")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập Email")]
+        [EmailAddress(ErrorMessage = "Email không hợp lệ")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập Số điện thoại")]
+        [Phone(ErrorMessage = "Số điện thoại không hợp lệ")]
+        [Display(Name = "Số điện thoại")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng chọn nhu cầu")]
+        [Display(Name = "Bạn quan tâm đến?")]
+        public string Subject { get; set; } // dropdown (Tư vấn / Kiểm tra trình độ...)
+
+        [Display(Name = "Lời nhắn / Câu hỏi thêm")]
+        public string? Message { get; set; }
+    }
+}
